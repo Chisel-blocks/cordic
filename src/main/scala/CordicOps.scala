b51b8845d4d12da7ac115d621f145e65649f47fd
Aleksi Korsman <aleksi.korsman@aalto.fi>
2023-10-12 10:02:51 +0300

    Add subtraction option

--- src/main/scala/CordicOps.scala
+++ src/main/scala/CordicOps.scala
@@ -8,22 +8,24 @@ trait CordicInfo {
   val iterations: Int
 }
 
-object CordicRegister extends Enumeration {
+object CordicRegister extends ChiselEnum {
   val rs1, rs2 = Value
 }
 
-object CordicResultRegister extends Enumeration {
+object CordicResultRegister extends ChiselEnum {
   val x, y, z = Value
 }
 
-case class CordicInitAdd(val addA: CordicRegister.Value, val addB: Double)
+abstract class CordicInit(val addA: CordicRegister.Type, val addB: Double)
+class CordicInitAdd(addA: CordicRegister.Type, addB: Double) extends CordicInit(addA, addB)
+class CordicInitSub(addA: CordicRegister.Type, addB: Double) extends CordicInit(addA, addB)
 
 abstract class CordicOp extends CordicInfo {
   // Fields that child classes have to implement
-  val xInit: Either[Double, CordicInitAdd]
-  val yInit: Either[Double, CordicInitAdd]
-  val zInit: Either[Double, CordicInitAdd]
-  val resReg: CordicResultRegister.Value
+  val xInit: Either[Double, CordicInit]
+  val yInit: Either[Double, CordicInit]
+  val zInit: Either[Double, CordicInit]
+  val resReg: CordicResultRegister.Type
   val cordicMode: CordicMode.Type
   val rotationType: CordicRotationType.Type
 }
@@ -31,7 +33,7 @@ abstract class CordicOp extends CordicInfo {
 abstract class CordicSine extends CordicOp {
   override val xInit        = Left(CordicMethods.calcK(iterations, CordicRotationType.CIRCULAR))
   override val yInit        = Left(0.0)
-  override val zInit        = Right(CordicInitAdd(CordicRegister.rs1, 0))
+  override val zInit        = Right(new CordicInitAdd(CordicRegister.rs1, 0))
   override val resReg       = CordicResultRegister.y
   override val cordicMode   = CordicMode.ROTATION
   override val rotationType = CordicRotationType.CIRCULAR
@@ -40,7 +42,16 @@ abstract class CordicSine extends CordicOp {
 abstract class CordicCosine extends CordicOp {
   override val xInit        = Left(CordicMethods.calcK(iterations, CordicRotationType.CIRCULAR))
   override val yInit        = Left(0.0)
-  override val zInit        = Right(CordicInitAdd(CordicRegister.rs1, 0))
+  override val zInit        = Right(new CordicInitAdd(CordicRegister.rs1, 0))
+  override val resReg       = CordicResultRegister.x
+  override val cordicMode   = CordicMode.ROTATION
+  override val rotationType = CordicRotationType.CIRCULAR
+}
+
+abstract class CordicLog extends CordicOp {
+  override val xInit        = Right(new CordicInitAdd(CordicRegister.rs1, 1.0))
+  override val yInit        = Right(new CordicInitSub(CordicRegister.rs1, 1.0))
+  override val zInit        = Left(0.0)
   override val resReg       = CordicResultRegister.x
   override val cordicMode   = CordicMode.ROTATION
   override val rotationType = CordicRotationType.CIRCULAR
