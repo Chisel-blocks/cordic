e51ae43c1453ac8e5121608d726245679df111a0
Aleksi Korsman <aleksi.korsman@aalto.fi>
2024-03-12 13:59:03 +0200

    Modify register structure

--- src/main/scala/AXI4LCordic.scala
+++ src/main/scala/AXI4LCordic.scala
@@ -19,24 +19,24 @@ class AXI4LCordic(
 
   val cordic = Module(new CordicTop(config))
 
-  val inRegs = Reg(Vec(3, UInt(32.W)))
+  val inRegs = Reg(Vec(2, UInt(32.W)))
   val outRegs = Reg(Vec(3, UInt(32.W)))
-  cordic.io.in.bits.rs1 := inRegs(0).asSInt
-  cordic.io.in.bits.rs2 := inRegs(1).asSInt
-  cordic.io.in.bits.rs3 := inRegs(2).asSInt
+  cordic.io.in.bits.rs1 := inRegs(0)(15,0).asSInt
+  cordic.io.in.bits.rs2 := inRegs(0)(31,16).asSInt
+  cordic.io.in.bits.rs3 := inRegs(1).asSInt
   outRegs(0) := cordic.io.out.bits.cordic.x.asUInt
   outRegs(1) := cordic.io.out.bits.cordic.y.asUInt
   outRegs(2) := cordic.io.out.bits.cordic.z.asUInt
   // Memory map for input and output registers
   val memory_map = Map[Data, MemoryRange](
-    inRegs(0) -> MemoryRange(begin = 0, end = 3, mode = MemoryMode.W, name = "x_in"),
-    inRegs(1) -> MemoryRange(begin = 4, end = 7, mode = MemoryMode.W, name = "y_in"),
-    inRegs(2) -> MemoryRange(begin = 8, end = 11, mode = MemoryMode.W, name = "z_in"),
-    outRegs(0) -> MemoryRange(begin = 12, end = 15, mode = MemoryMode.R, name = "x_out"),
-    outRegs(1) -> MemoryRange(begin = 16, end = 19, mode = MemoryMode.R, name = "y_out"),
-    outRegs(2) -> MemoryRange(begin = 20, end = 23, mode = MemoryMode.R, name = "z_out"),
-    cordic.io.in.bits.control -> MemoryRange(begin = 24, end = 27, mode = MemoryMode.RW, name = "control"),
-    cordic.io.in.valid -> MemoryRange(begin = 28, end = 31, mode = MemoryMode.RW, name = "control_2"),
+    inRegs(0) -> MemoryRange(begin = 0, end = 3, mode = MemoryMode.W, name = "x_y_in"),
+    inRegs(1) -> MemoryRange(begin = 4, end = 7, mode = MemoryMode.W, name = "z_in"),
+    outRegs(0) -> MemoryRange(begin = 8, end = 11, mode = MemoryMode.R, name = "x_out", stall_sig = !cordic.io.out.valid),
+    outRegs(1) -> MemoryRange(begin = 12, end = 15, mode = MemoryMode.R, name = "y_out", stall_sig = !cordic.io.out.valid),
+    outRegs(2) -> MemoryRange(begin = 16, end = 19, mode = MemoryMode.R, name = "z_out", stall_sig = !cordic.io.out.valid),
+    cordic.io.in.bits.control -> MemoryRange(begin = 20, end = 23, mode = MemoryMode.RW, name = "control"),
+    cordic.io.in.valid -> MemoryRange(begin = 24, end = 27, mode = MemoryMode.RW, name = "valid_in"),
+    cordic.io.out.valid -> MemoryRange(begin = 28, end = 31, mode = MemoryMode.R, name = "valid_out"),
   )
 
   buildAxi4LiteSlaveLogic()
